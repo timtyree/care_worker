822e684ada48c91f3a52ca346cf0af7c17cebfe8
Tim Tyree <tjt57@cornell.edu>
2021-01-31 20:53:06 -0800

    updated .gitignore and usage of runs/

--- python/make_worker_env.sh
+++ python/make_worker_env.sh
@@ -4,7 +4,7 @@ python3 -m venv worker_env
 source worker_env/bin/activate
 python3 -m pip install --upgrade pip
 python3 -m pip install numba
-python3 -m pip install scikit-image trackpy
+python3 -m pip install scikit-image trackpy scikit-learn
 python3 -m pip install pandas scipy matplotlib
 tar czf worker_env.tar.gz worker_env
 deactivate
